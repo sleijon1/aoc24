c3b1a656179e3a852243d0c6a557f65e1c5cc072
Simon Leijon <simonleijon59@gmail.com>
2024-12-03 18:30:00 +0100

    d3 donezo

--- days/d3/d3.py
+++ days/d3/d3.py
@@ -1,1 +1,16 @@
-lines = open("input.txt").read().splitlines()
+import re
+import math
+text = open("input.txt").read()
+pattern = r"mul\(\d+,\d+\)|don't\(\)|do\(\)"
+matches = re.findall(pattern, text)
+remove, dont = [], False
+for i, match in enumerate(matches):
+    if match == "don't()":
+        dont = True
+    elif match == "do()":
+        dont = False
+    elif dont:
+        remove.append(i)
+p1 = sum([math.prod(map(int, match[4:-1].split(","))) for i, match in enumerate(matches) if "do" not in match])
+p2 = sum([math.prod(map(int, match[4:-1].split(","))) for i, match in enumerate(matches) if i not in remove and "do" not in match])
+print(f"Part 1: {p1}\nPart 2: {p2}")
