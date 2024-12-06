e55be835061e959927a94ea5e1a8e7e034407248
Simon Leijon <simonleijon59@gmail.com>
2024-12-06 18:47:55 +0100

    removed noob copies - need further cleaning

--- days/d6/d6.py
+++ days/d6/d6.py
@@ -1,4 +1,3 @@
-from copy import deepcopy
 lines = [list(line) for line in open("input.txt").read().splitlines()]
 obstacles, guard, possible_obstacles = [], None, []
 for y, line in enumerate(lines):
@@ -11,17 +10,12 @@ for y, line in enumerate(lines):
             possible_obstacles.append((x, y))
 
 
-original_obstacles = deepcopy(obstacles)
-original_guard = guard
-original_lines = deepcopy(lines)
+def path_guard(obstacles, guard, lines, direction):
+    stuck, traversed, visited = 0, set(), set()
 
-def path_guard(obstacles, guard, lines):
-    traversed = set()
-    stuck = 0
-    round = 0
     while True:
             obstacles_in_the_way = []
-            if lines[guard[1]][guard[0]] == ">":
+            if direction == ">":
                 obstacles_in_the_way = sorted([obstacle for obstacle in obstacles if obstacle[0] > guard[0] and obstacle[1] == guard[1]], key=lambda x: x[0])
                 if not obstacles_in_the_way:
                     for x in range(guard[0], len(lines[0])):
@@ -30,8 +24,13 @@ def path_guard(obstacles, guard, lines):
                 for x in range(guard[0], obstacles_in_the_way[0][0]):
                     traversed.add((x, guard[1]))
                 guard = (obstacles_in_the_way[0][0]-1, guard[1])
-                lines[guard[1]][guard[0]] = "v"
-            elif lines[guard[1]][guard[0]] == "<":
+                direction = "v"
+                if (obstacles_in_the_way[0][0], obstacles_in_the_way[0][1], ">") not in visited:
+                    visited.add((obstacles_in_the_way[0][0], obstacles_in_the_way[0][1], ">"))
+                else:
+                    stuck += 1
+                    break
+            elif direction == "<":
                 obstacles_in_the_way = sorted([obstacle for obstacle in obstacles if obstacle[0] < guard[0] and obstacle[1] == guard[1]], key=lambda x: x[0], reverse=True)
                 if not obstacles_in_the_way:
                     for x in range(guard[0], 0):
@@ -40,8 +39,13 @@ def path_guard(obstacles, guard, lines):
                 for x in range(guard[0], obstacles_in_the_way[0][0], -1):
                     traversed.add((x, guard[1]))
                 guard = (obstacles_in_the_way[0][0]+1, guard[1])
-                lines[guard[1]][guard[0]] = "^"
-            elif lines[guard[1]][guard[0]] == "^":
+                direction = "^"
+                if (obstacles_in_the_way[0][0], obstacles_in_the_way[0][1], "<") not in visited:
+                    visited.add((obstacles_in_the_way[0][0], obstacles_in_the_way[0][1], "<"))
+                else:
+                    stuck += 1
+                    break
+            elif direction == "^":
                 obstacles_in_the_way = sorted([obstacle for obstacle in obstacles if obstacle[1] < guard[1] and obstacle[0] == guard[0]], key=lambda x: x[1], reverse=True)
                 if not obstacles_in_the_way:
                     for y in range(guard[1], 0):
@@ -50,8 +54,13 @@ def path_guard(obstacles, guard, lines):
                 for y in range(guard[1], obstacles_in_the_way[0][1], -1):
                     traversed.add((guard[0], y))
                 guard = (guard[0], obstacles_in_the_way[0][1]+1)
-                lines[guard[1]][guard[0]] = ">"
-            elif lines[guard[1]][guard[0]] == "v":
+                direction = ">"
+                if (obstacles_in_the_way[0][0], obstacles_in_the_way[0][1], "^") not in visited:
+                    visited.add((obstacles_in_the_way[0][0], obstacles_in_the_way[0][1], "^"))
+                else:
+                    stuck += 1
+                    break
+            elif direction == "v":
                 obstacles_in_the_way = sorted([obstacle for obstacle in obstacles if obstacle[1] > guard[1] and obstacle[0] == guard[0]], key=lambda x: x[1])
                 if not obstacles_in_the_way:
                     for y in range(guard[0], len(lines)):
@@ -60,25 +69,22 @@ def path_guard(obstacles, guard, lines):
                 for y in range(guard[1], obstacles_in_the_way[0][1]):
                     traversed.add((guard[0], y))
                 guard = (guard[0], obstacles_in_the_way[0][1]-1)
-                lines[guard[1]][guard[0]] = "<"
+                direction = "<"
+                if (obstacles_in_the_way[0][0], obstacles_in_the_way[0][1], "v") not in visited:
+                    visited.add((obstacles_in_the_way[0][0], obstacles_in_the_way[0][1], "v"))
+                else:
+                    stuck += 1
+                    print(f"found stuck at {x}, {y}")
+                    break
             if not obstacles_in_the_way:
                 break
-            if round > 300:
-                stuck += 1
-                print(f"found stuck at {x}, {y}")
-                break
-            round += 1
     return stuck, len(traversed)
 
 stuck = 0
 for (x,y) in possible_obstacles:
-    obstacles = deepcopy(original_obstacles)
-    obstacles.append((x, y))
     round = 0
-    guard = original_guard
-    lines = deepcopy(original_lines)
-    path_stuck, traversed = path_guard(obstacles, guard, lines)
+    path_stuck, traversed = path_guard(obstacles+[(x, y)], guard, lines, direction=lines[guard[1]][guard[0]])
     stuck += path_stuck
 
-_, traversed = path_guard(deepcopy(original_obstacles), original_guard, deepcopy(original_lines))    
+_, traversed = path_guard(obstacles, guard, lines, direction=lines[guard[1]][guard[0]])    
 print(f"Part 1: {traversed}, Part 2: {stuck}")
